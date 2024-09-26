7ddff141237180f6caa95382e7a0f0ad549e6b7e
Yuri Filgueira <yufsdev@gmail.com>
2024-09-26 20:36:49 -0300

    atualização do spider e do arquivo pipelines.py

--- mhw/pipelines.py
+++ mhw/pipelines.py
@@ -11,3 +11,14 @@ from itemadapter import ItemAdapter
 class MhwPipeline:
     def process_item(self, item, spider):
         return item
+
+class MonsterPipeline:
+    def process_item(self, item, spider):
+        for part in item['parts']:
+            for key, value in part.items():
+                if value is None:
+                    part[key] = 'N/A'  # Atribui 'N/A' para valores faltantes
+                elif key in ['corte', 'contusao', 'a_distancia', 'fogo', 'agua', 'gelo', 'trovao', 'dragao', 'status', 'vigor']:
+                    part[key] = int(value) if value.isdigit() else 0  # Converte para inteiro ou atribui 0 se não for número
+
+        return item
--- mhw/spiders/monster_spider.py
+++ mhw/spiders/monster_spider.py
@@ -3,6 +3,9 @@ import scrapy
 class MonsterSpider(scrapy.Spider):
     name = 'monster_spider'
     start_urls = ['https://mhworld.kiranico.com/pt-BR/monsters']
+    custom_settings = {
+        'LOG_LEVEL': 'DEBUG',
+    }
 
     def parse(self, response):
         for monster in response.css('table.table-padded > tbody > tr'):
@@ -11,23 +14,44 @@ class MonsterSpider(scrapy.Spider):
                 yield response.follow(url, self.parse_monster)
 
     def parse_monster(self, response):
-        name = response.css('h5 > div > div.align-self-center::text').get().strip()
-        description = response.css('div.col-sm-6::text').get().strip()
+        name = response.css('h5 > div > div.align-self-center::text').get()
+        description = response.css('div.col-sm-6::text').get()
+
+        if name is None or description is None:
+            self.logger.error(f"Nome ou descrição não encontrado para a URL: {response.url}")
+            return
+
+        name = name.strip()
+        description = description.strip()
+        
         table = response.css('div.table-responsive table.table')
+        parts_data = []
 
         for row in table.css('tbody tr'):
-            yield {
-                'name': name,
-                'description': description,
-                'part': row.css('td:nth-child(1)::text').get(),
-                'corte': row.css('td:nth-child(2)::text').get(),
-                'contusao': row.css('td:nth-child(3)::text').get(),
-                'a_distancia': row.css('td:nth-child(4)::text').get(),
-                'fogo': row.css('td:nth-child(5)::text').get(),
-                'agua': row.css('td:nth-child(6)::text').get(),
-                'gelo': row.css('td:nth-child(7)::text').get(),
-                'trovao': row.css('td:nth-child(8)::text').get(),
-                'dragao': row.css('td:nth-child(9)::text').get(),
-                'status': row.css('td:nth-child(10)::text').get(),
-                'vigor': row.css('td:nth-child(11)::text').get()
+            part = row.css('td:nth-child(1)::text').get()
+            if part:
+                part_data = {
+                    'part': part.strip,
+                    'corte': row.css('td:nth-child(2)::text').get().strip(),
+                    'contusao': row.css('td:nth-child(3)::text').get().strip(),
+                    'a_distancia': row.css('td:nth-child(4)::text').get().strip(),
+                    'fogo': row.css('td:nth-child(5)::text').get().strip(),
+                    'agua': row.css('td:nth-child(6)::text').get().strip(),
+                    'gelo': row.css('td:nth-child(7)::text').get().strip(),
+                    'trovao': row.css('td:nth-child(8)::text').get().strip(),
+                    'dragao': row.css('td:nth-child(9)::text').get().strip(),
+                    'status': row.css('td:nth-child(10)::text').get().strip(),
+                    'vigor': row.css('td:nth-child(11)::text').get().strip()
+                }
+                parts_data.append(part_data)
+            else:
+                self.logger.error(f"Parte não encontrada na URL: {response.url}")
+
+        if parts_data:
+            yield{
+                'name': name.strip(),
+                'description': description.strip(),
+                'parts': parts_data
             }
+        else:
+            self.logger.warning(f"Nenhuma parte encontrada para o monstro: {name}")
