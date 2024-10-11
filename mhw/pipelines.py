4b0b8af547ef44dddac2367ade5cbfb19be07e21
Yuri Filgueira <yufsdev@gmail.com>
2024-10-11 20:44:05 -0300

    tudo ok

--- mhw/pipelines.py
+++ mhw/pipelines.py
@@ -6,6 +6,7 @@
 
 # useful for handling different item types with a single interface
 #from itemadapter import ItemAdapter
+import os
 import pandas as pd
 
 class MhwPipeline:
@@ -13,19 +14,11 @@ class MhwPipeline:
         return item
 
 class MonsterPipeline:
-    part_translation = {
-        'head': 'cabeça',
-        'tail': 'cauda',
-        'wing': 'asa',
-        'claw': 'garra',
-        # Adicione mais traduções conforme necessário
-    }
-
     def process_item(self, item, spider):
         for part in item['parts']:
             part_name = part['part']
-            if part_name in self.part_translation:
-                part['part'] = self.part_translation[part_name]
+            if not part_name:
+                part['part'] = f'iceborne {part_name.strip()}'
 
             for key, value in part.items():
                 if value is None:
@@ -40,9 +33,47 @@ class MonsterVisualizationPipeline:
         # Inicializa uma lista para armazenar os itens coletados
         self.data = []
 
+        # Define uma lista de partes permitidas em inglês
+        self.allowed_parts = ['Head', 'Neck', 'Tail', 'Body', 'Wings', 'Legs', 'Arms', 'Jaw', 'Back', 'Head Bone', 'Hindleg Bones', 'Left Bone', 'Right Bone']
+        self.physiology_data = []
+        self.breakability_data = []
+
     def process_item(self, item, spider):
-        # Adiciona cada item processado na lista de dados
-        self.data.append(item)
+        filtered_parts = []
+        
+        for part in item['parts']:
+            # Converte os valores que devem ser numéricos para um array
+            numerical_values = [
+                part.get('corte', 0),
+                part.get('contusao', 0),
+                part.get('a_distancia', 0),
+                part.get('fogo', 0),
+                part.get('agua', 0),
+                part.get('gelo', 0),
+                part.get('trovao', 0),
+                part.get('dragao', 0),
+                part.get('status', 0),
+                part.get('vigor', 0)
+            ]
+            
+            # Verifica se todos os valores numéricos são zero
+            if any(value != 0 for value in numerical_values):
+                filtered_parts.append(part)
+
+        # Se houver partes válidas (não totalmente zeradas), adiciona o item
+        if filtered_parts:
+            item['parts'] = filtered_parts
+            self.data.append(item)
+
+        # Separar os dados de physiology e de part breakability
+        physiology = item.get('physiology')
+        if physiology:
+            self.physiology_data.append(physiology)
+
+        breakability = item.get('breakability')
+        if breakability:
+            self.breakability_data.append(breakability)
+
         return item
 
     def close_spider(self, spider):
@@ -60,9 +91,28 @@ class MonsterVisualizationPipeline:
                 }
                 all_monster_data.append(part_data)
 
-        # Cria um DataFrame com os dados coletados
-        df = pd.DataFrame(all_monster_data)
+        # Cria DataFrames com os dados coletados
+        df_monsters = pd.DataFrame(all_monster_data)
+        df_physiology = pd.DataFrame(self.physiology_data)
+        df_breakability = pd.DataFrame(self.breakability_data)
 
-        # Salva os dados em um arquivo CSV
-        df.to_csv('monsters.csv', index=False)
-        spider.logger.info("Arquivo 'monsters.csv' salvo com sucesso.")
+        # Define o caminho para o diretório fora do projeto
+        output_directory = '/home/yuri/monsters_csv'
+
+        # Verifica se o diretório já existe, se não, cria o diretório
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        # Define o caminho completo dos arquivos CSV
+        file_monsters = os.path.join(output_directory, 'monsters.csv')
+        file_physiology = os.path.join(output_directory, 'physiology.csv')
+        file_breakability = os.path.join(output_directory, 'breakability.csv')
+
+        # Salva os dados em arquivos CSV
+        df_monsters.to_csv(file_monsters, index=False)
+        df_physiology.to_csv(file_physiology, index=False)
+        df_breakability.to_csv(file_breakability, index=False)
+
+        spider.logger.info(f"Arquivo 'monsters.csv' salvo com sucesso em {file_monsters}")
+        spider.logger.info(f"Arquivo 'physiology.csv' salvo com sucesso em {file_physiology}")
+        spider.logger.info(f"Arquivo 'breakability.csv' salvo com sucesso em {file_breakability}")
