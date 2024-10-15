aaee0f841adc3fbb3e518709b734ea011cbec64b
Yuri Filgueira <yufsdev@gmail.com>
2024-10-15 09:45:19 -0300

    ajustes nas tabelas

--- mhw/pipelines.py
+++ mhw/pipelines.py
@@ -1,89 +1,89 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-#from itemadapter import ItemAdapter
 import os
 import pandas as pd
 
-class MhwPipeline:
-    def process_item(self, item, spider):
-        return item
-
 class MonsterPipeline:
+    allowed_parts = [
+        'Head', 'Neck', 'Left Leg', 'Right Leg', 'Tail', 'Body', 'Wings', 
+        'Legs', 'Arms', 'Jaw', 'Back', 'Torso', 'Leg', 'Nose', 'Left Foreleg', 
+        'Right Foreleg', 'Left Hindleg', 'Right Hindleg', 'Forelegs'
+    ]
+
     def process_item(self, item, spider):
+        filtered_parts = []
+
         for part in item['parts']:
             part_name = part['part']
-            if not part_name:
-                part['part'] = f'iceborne {part_name.strip()}'
 
-            for key, value in part.items():
-                if value is None:
-                    part[key] = 'N/A'  # Atribui 'N/A' para valores faltantes
-                elif key in ['corte', 'contusao', 'a_distancia', 'fogo', 'agua', 'gelo', 'trovao', 'dragao', 'status', 'vigor']:
-                    part[key] = int(value) if value.isdigit() else 0  # Converte para inteiro ou atribui 0 se não for número
+            if part_name in self.allowed_parts:
+                for key, value in part.items():
+                    if value is None:
+                        part[key] = 'iceborne ' + part_name  # Substitui valores vazios
+                    elif key in ['corte', 'contusao', 'a_distancia', 'fogo', 'agua', 'trovao', 'gelo', 'dragao', 'atordoamento', 'vigor']:
+                        part[key] = int(value) if isinstance(value, str) and value.isdigit() else value or 0
+
+                filtered_parts.append(part)
+
+        if filtered_parts:
+            item['parts'] = filtered_parts
+        else:
+            item['parts'] = []
 
         return item
 
 class MonsterVisualizationPipeline:
     def open_spider(self, spider):
-        # Inicializa uma lista para armazenar os itens coletados
         self.data = []
-
-        # Define uma lista de partes permitidas em inglês
-        self.allowed_parts = ['Head', 'Neck', 'Tail', 'Body', 'Wings', 'Legs', 'Arms', 'Jaw', 'Back', 'Head Bone', 'Hindleg Bones', 'Left Bone', 'Right Bone']
         self.physiology_data = []
-        self.breakability_data = []
 
     def process_item(self, item, spider):
         filtered_parts = []
-        
+
         for part in item['parts']:
-            # Converte os valores que devem ser numéricos para um array
             numerical_values = [
                 part.get('corte', 0),
                 part.get('contusao', 0),
                 part.get('a_distancia', 0),
                 part.get('fogo', 0),
                 part.get('agua', 0),
-                part.get('gelo', 0),
                 part.get('trovao', 0),
+                part.get('gelo', 0),
                 part.get('dragao', 0),
-                part.get('status', 0),
+                part.get('atordoamento', 0),
                 part.get('vigor', 0)
             ]
-            
-            # Verifica se todos os valores numéricos são zero
+
             if any(value != 0 for value in numerical_values):
                 filtered_parts.append(part)
 
-        # Se houver partes válidas (não totalmente zeradas), adiciona o item
+                # Dados de fisiologia
+                physiology = {
+                    'name': item['name'],
+                    'part': part['part'],
+                    'corte': part.get('corte', 0),
+                    'contusao': part.get('contusao', 0),
+                    'a_distancia': part.get('a_distancia', 0),
+                    'fogo': part.get('fogo', 0),
+                    'agua': part.get('agua', 0),
+                    'trovao': part.get('trovao', 0),
+                    'gelo': part.get('gelo', 0),
+                    'dragao': part.get('dragao', 0),
+                    'atordoamento': part.get('atordoamento', 0),
+                    'vigor': part.get('vigor', 0)
+                }
+                self.physiology_data.append(physiology)
+
         if filtered_parts:
             item['parts'] = filtered_parts
             self.data.append(item)
 
-        # Separar os dados de physiology e de part breakability
-        physiology = item.get('physiology')
-        if physiology:
-            self.physiology_data.append(physiology)
-
-        breakability = item.get('breakability')
-        if breakability:
-            self.breakability_data.append(breakability)
-
         return item
 
     def close_spider(self, spider):
-        # Converte a lista de dados em um DataFrame do pandas
         all_monster_data = []
         for item in self.data:
             name = item['name']
-            description = item['description']
+            description = item.get('description', '')
             for part in item['parts']:
-                # Adiciona o nome do monstro e a descrição em cada parte
                 part_data = {
                     'name': name,
                     'description': description,
@@ -91,28 +91,23 @@ class MonsterVisualizationPipeline:
                 }
                 all_monster_data.append(part_data)
 
-        # Cria DataFrames com os dados coletados
-        df_monsters = pd.DataFrame(all_monster_data)
+        df_monsters = pd.DataFrame(self.data, columns=['name', 'description'])
         df_physiology = pd.DataFrame(self.physiology_data)
-        df_breakability = pd.DataFrame(self.breakability_data)
 
-        # Define o caminho para o diretório fora do projeto
         output_directory = '/home/yuri/monsters_csv'
 
-        # Verifica se o diretório já existe, se não, cria o diretório
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
 
-        # Define o caminho completo dos arquivos CSV
-        file_monsters = os.path.join(output_directory, 'monsters.csv')
-        file_physiology = os.path.join(output_directory, 'physiology.csv')
-        file_breakability = os.path.join(output_directory, 'breakability.csv')
+        file_path_monsters = os.path.join(output_directory, 'monsters.csv')
+        file_path_physiology = os.path.join(output_directory, 'physiology.csv')
 
-        # Salva os dados em arquivos CSV
-        df_monsters.to_csv(file_monsters, index=False)
-        df_physiology.to_csv(file_physiology, index=False)
-        df_breakability.to_csv(file_breakability, index=False)
+        # Reordena colunas de physiology.csv
+        columns_order_physiology = ['name', 'part', 'corte', 'contusao', 'a_distancia', 'fogo', 'agua', 'trovao', 'gelo', 'dragao', 'atordoamento', 'vigor']
+        df_physiology = df_physiology[columns_order_physiology]
 
-        spider.logger.info(f"Arquivo 'monsters.csv' salvo com sucesso em {file_monsters}")
-        spider.logger.info(f"Arquivo 'physiology.csv' salvo com sucesso em {file_physiology}")
-        spider.logger.info(f"Arquivo 'breakability.csv' salvo com sucesso em {file_breakability}")
+        df_monsters.to_csv(file_path_monsters, index=False)
+        df_physiology.to_csv(file_path_physiology, index=False)
+
+        spider.logger.info("Arquivos 'monsters.csv' e 'physiology.csv' salvos com sucesso.")
+
--- mhw/spiders/monster_spider.py
+++ mhw/spiders/monster_spider.py
@@ -3,55 +3,78 @@ import scrapy
 class MonsterSpider(scrapy.Spider):
     name = 'monster_spider'
     start_urls = ['https://mhworld.kiranico.com/pt-BR/monsters']
+
     custom_settings = {
         'LOG_LEVEL': 'DEBUG',
     }
 
     def parse(self, response):
+        self.logger.info("Iniciando a coleta de monstros...")
+        
+        # Extrai a lista de monstros e segue os links para as páginas detalhadas
         for monster in response.css('table.table-padded > tbody > tr'):
             url = monster.css('td div span a::attr(href)').get()
+            name = monster.css('td div span a::text').get()
+            
             if url:
+                self.logger.debug(f"Seguindo o link do monstro: {name} - {url}")
                 yield response.follow(url, self.parse_monster)
+            else:
+                self.logger.warning(f"Nenhum URL encontrado para o monstro: {name}")
 
     def parse_monster(self, response):
-        name = response.css('h5 > div > div.align-self-center::text').get()
-        description = response.css('div.col-sm-6::text').get()
-
-        if name is None or description is None:
-            self.logger.error(f"Nome ou descrição não encontrado para a URL: {response.url}")
-            return
-
-        name = name.strip()
-        description = description.strip()
-        
-        table = response.css('div.table-responsive table.table')
-        parts_data = []
-
-        for row in table.css('tbody tr'):
-            part = row.css('td:nth-child(1)::text').get()
-            if part:
-                part_data = {
-                    'part': part.strip(),
-                    'corte': (row.css('td:nth-child(2)::text').get() or '').strip(),
-                    'contusao': (row.css('td:nth-child(3)::text').get() or '').strip(),
-                    'a_distancia': (row.css('td:nth-child(4)::text').get() or '').strip(),
-                    'fogo': (row.css('td:nth-child(5)::text').get() or '').strip(),
-                    'agua': (row.css('td:nth-child(6)::text').get() or '').strip(),
-                    'gelo': (row.css('td:nth-child(7)::text').get() or '').strip(),
-                    'trovao': (row.css('td:nth-child(8)::text').get() or '').strip(),
-                    'dragao': (row.css('td:nth-child(9)::text').get() or '').strip(),
-                    'status': (row.css('td:nth-child(10)::text').get() or '').strip(),
-                    'vigor': (row.css('td:nth-child(11)::text').get() or '').strip()
-                }
-                parts_data.append(part_data)
-            else:
-                self.logger.error(f"Parte não encontrada na URL: {response.url}")
-
-        if parts_data:
-            yield{
-                'name': name.strip(),
-                'description': description.strip(),
-                'parts': parts_data
-            }
+        # Extrai nome e descrição do monstro
+        name = response.css('div.align-self-center::text').get()
+        if name:
+            name = name.strip()
         else:
-            self.logger.warning(f"Nenhuma parte encontrada para o monstro: {name}")
+            self.logger.warning("Nome do monstro não encontrado.")
+
+        description = response.css('div.col-sm-6::text').get()
+        if description:
+            description = description.strip()
+        else:
+            self.logger.warning(f"Descrição não encontrada para o monstro: {name}")
+
+        # Captura as tabelas de fisiologia para diferentes elementos
+        elements = ["Fire", "Ice", "Dragon", "No Element"]
+        physiology_data = []
+
+        for element in elements:
+            self.logger.debug(f"Capturando fisiologia para o elemento: {element}")
+            table = response.xpath(f'//h6[contains(text(), "Fisiologia")]/following-sibling::div[@class="table-responsive"][1]')
+            if not table:
+                self.logger.warning(f"Tabela de fisiologia não encontrada para o elemento: {element}")
+                continue
+            
+            # Processa as partes do corpo e suas resistências para o elemento atual
+            for row in table.css('tbody tr'):
+                part = row.css('td:nth-child(1)::text').get()
+                if part:
+                    part_data = {
+                        'part': part.strip(),
+                        'element': element,  # Adiciona o elemento atual ao dicionário
+                        'corte': (row.css('td:nth-child(2)::text').get() or '').strip(),
+                        'contusao': (row.css('td:nth-child(3)::text').get() or '').strip(),
+                        'a_distancia': (row.css('td:nth-child(4)::text').get() or '').strip(),
+                        'fogo': (row.css('td:nth-child(5)::text').get() or '').strip(),
+                        'agua': (row.css('td:nth-child(6)::text').get() or '').strip(),
+                        'trovao': (row.css('td:nth-child(7)::text').get() or '').strip(),
+                        'gelo': (row.css('td:nth-child(8)::text').get() or '').strip(),
+                        'dragao': (row.css('td:nth-child(9)::text').get() or '').strip(),
+                        'atordoamento': (row.css('td:nth-child(10)::text').get() or '').strip(),
+                        'vigor': (row.css('td:nth-child(11)::text').get() or '').strip(),
+                    }
+                    physiology_data.append(part_data)
+
+        if physiology_data:
+            self.logger.debug(f"Fisiologia extraída para o monstro: {name}")
+        else:
+            self.logger.warning(f"Nenhuma parte de fisiologia encontrada para o monstro: {name}")
+
+        yield {
+            'name': name,
+            'description': description,
+            'physiology': physiology_data  # Dados de fisiologia completos, incluindo múltiplos elementos
+        }
+
